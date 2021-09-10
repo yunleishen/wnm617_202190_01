5ffd079b019d561dfa9d2472a6a630160aa6b7a6
Yunlei <yunleishen@Yunleis-MacBook-Pro.local>
2021-09-10 13:52:30 -0700

    Created index page. Created about page for portfolio.

---
+++ shen.yunlei/parts/meta.php
@@ -0,0 +1,12 @@
+	
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<base href="http://yunleishen.net/elaine/aau/wnm617/shen.yunlei/">
+
+	<link rel="stylesheet" href="https://use.typekit.net/umv3skt.css">
+	<link rel="stylesheet" href="CSS/style.css">
+	<link rel="stylesheet" href="lib/CSS/gridsystem.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
