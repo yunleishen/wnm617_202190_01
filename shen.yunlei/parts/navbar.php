0e7021cb7b272440dc5351a2070c29f01fa01b70
Yunlei <yunleishen@Yunleis-MacBook-Pro.local>
2021-09-10 14:11:25 -0700

    Created index page. Created about page for portfolio.(modified)

--- shen.yunlei/parts/navbar.php
+++ shen.yunlei/parts/navbar.php
@@ -2,7 +2,7 @@
 <header class="navbar">
 		<div class="container-lg display-flex">
 			<div class="flex-none">
-				<div id="logo" onclick="location.href='portfolio-index.php'">YUNLEI SHEN</div>
+				<div class ="logo" onclick="location.href='portfolio-index.php'">YUNLEI SHEN</div>
 			</div>
 
 
@@ -12,7 +12,7 @@
 				<ul class="container display-flex">
 					<li><a href="portfolio-work.php">WORK</a></li>
 					<li><a href="portfolio-about.php">ABOUT</a></li>
-					<li><a href="#">RESUME</a></li>
+					<li><a href="">RESUME</a></li>
 				</ul>
 			</div>
 		</div>
--- shen.yunlei/portfolio-about.php
+++ shen.yunlei/portfolio-about.php
@@ -19,7 +19,7 @@
 				<p>Hi, nice to meet you, I am <b>Yunlei</b>!</p>
 			</div>
 			<div class="intro-p2">
-				<p>A designer hoping a inspiration life through enjoyable interactions.</p>
+				<p>A designer hoping an inspiration life through enjoyable interactions.</p>
 				<p>I'm currently a student at the Academy of Art University, purchasing a Master's Degree in Web Design & New Media.</p>
 			</div>
 			
