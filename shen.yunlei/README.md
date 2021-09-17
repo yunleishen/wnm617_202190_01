ba23a3eb75c0dc589682a0704a55e772270b1456
Yunlei <yunleishen@Yunleis-MacBook-Pro.local>
2021-09-17 03:07:37 -0700

    initializr CSS

--- shen.yunlei/README.md
+++ shen.yunlei/README.md
@@ -5,6 +5,9 @@
 - http://yunleishen.net
 - http://yunleishen.net/elaine/aau/wnm617/shen.yunlei/
 
+### Side Assignments
+- http://yunleishen.net/elaine/aau/wnm617/shen.yunlei/initializr/
+
 ## Perhaps Some Notes
 
 - Take note of how markdown works
---
+++ shen.yunlei/portfolio/portfolio-about.php
@@ -0,0 +1,35 @@
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Portfolio</title>
+	<?php include "parts/meta.php"; ?>
+
+
+</head>
+<body class="flush">
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container-sm display-flex">
+		<div class="figure"><img src="img/avatar2.jpg" alt="" class="flex-none"></div>
+		<div class="flex-stretch"></div>
+		<div class="flex-none intro">
+			<div class="intro-p1">
+				<p>Hi, nice to meet you, I am <b>Yunlei</b>!</p>
+			</div>
+			<div class="intro-p2">
+				<p>A designer hoping an inspiration life through enjoyable interactions.</p>
+				<p>I'm currently a student at the Academy of Art University, purchasing a Master's Degree in Web Design & New Media.</p>
+			</div>
+			
+		</div>
+	</div>
+
+	
+
+
+	<?php include "parts/footer.php" ?>
+	
+</body>
+</html>
