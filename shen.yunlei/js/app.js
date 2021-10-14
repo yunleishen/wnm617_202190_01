00f09c9677812ea8fe0071615d67e421c65ec8c6
Yunlei <yunleishen@Yunleis-MacBook-Pro.local>
2021-10-13 21:09:35 -0700

    add animal page, animal profile page, location page, user profile page, add icons to the nav

--- shen.yunlei/js/app.js
+++ shen.yunlei/js/app.js
@@ -2,16 +2,23 @@
 // DOCUMENT READY
 $(()=>{
 
+   checkUserId();
+
    // Event Delegation
    $(document)
 
+
+   // FORM SUBMITS
    .on("submit","#signin-form",function(e) {
       e.preventDefault();
       checkSigninForm();
    })
+   .on("submit","#list-add-form",function(e) {
+      e.preventDefault();
+   })
 
 
-      // ANCHOR CLICKS
+   // ANCHOR CLICKS
    .on("click",".js-logout",function(e) {
       e.preventDefault();
       sessionStorage.removeItem("userId");
@@ -21,19 +28,35 @@ $(()=>{
 
 
    .on("click","[data-activate]",function(e){
-      e.preventDefault();
       let target = $(this).data("activate");
       $(target).addClass("active");
    })
    .on("click","[data-deactivate]",function(e){
-      e.preventDefault();
       let target = $(this).data("deactivate");
       $(target).removeClass("active");
    })
    .on("click","[data-toggle]",function(e){
-      e.preventDefault();
       let target = $(this).data("toggle");
       $(target).toggleClass("active");
    })
+   .on("click","[data-activateone]",function(e){
+      let target = $(this).data("activateone");
+      console.log(target)
+      $(target).addClass("active").siblings().removeClass('active');
+   })
+   ;
+
+
+   $("[data-template]").each(function(){
+      let target = $(this).data("template");
+      $(this).html($(target).html());
+   })
+
+   $({
+      "#page-home":".nav-icon-set li:nth-child(1)",
+      "#page-pets":".nav-icon-set li:nth-child(2)",
+      "#page-user":".nav-icon-set li:nth-child(3)",
+   }[location.hash]).addClass("active");
+
 
 });
